1954a2eb93582cf2367a9088f6b7b2912c2ec071
tuhin_4ever <tuhin4ever19@gmail.com>
2023-04-07 18:49:02 +0600

    Extract Cart products from Products data

--- src/Loaders/cartProductsLoader.js
+++ src/Loaders/cartProductsLoader.js
@@ -1,7 +1,29 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductsLoader = async () => {
     const loadedProducts = await fetch('products.json');
     const products = await loadedProducts.json();
-    console.log(products)
+    // console.log(products);
+
+    //* if cart data is inn database, you have to use async await
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+
+    console.log(storedCart);
+    for(const id in storedCart){
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct){
+            const quantity =storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    //* if you eed to send tow things
+    //* return [products, saveCart]
+    //* another options
+    //* return (products, cart: saveCart)
+
+    return savedCart;
 }
 
 export default cartProductsLoader;

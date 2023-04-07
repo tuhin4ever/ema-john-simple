fef51b3fcd7300327197c4079bc9ed10f383dac6
tuhin_4ever <tuhin4ever19@gmail.com>
2023-04-07 08:07:39 +0600

    React Router setup for Ema John

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Layout/Home';
+import Shop from './components/Shop/Shop';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+    children:[
+      {
+        path: '/',
+        element: <Shop/>
+      },
+      {
+        path: 'orders',
+        element: <Orders/>
+      },
+      {
+        path: 'inventory',
+        element: <Inventory/>
+      },
+      {
+        path: 'login',
+        element: <Login/>
+      }
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
